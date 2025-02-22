02ec47023ee1e6f462b48e17d2ff06864bf20fea
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2025-02-22 17:08:54 +0100

    Update tailwind.config.mjs
    Edit tailwind config for the principal colors , and text

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#1C1C1C", // Fondo oscuro elegante
+        textLight: "#F5F5F5", // Blanco cálido para contraste
+        textMuted: "#EAE7DC", // Tono arena, más suave
+        accent: "#D4A373", // Beige/dorado cálido
+        secondary: "#8BAAAD", // Azul grisáceo costero
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // Texto principal
+        serif: ["Playfair Display", "serif"], // Títulos
       },
     },
   },
