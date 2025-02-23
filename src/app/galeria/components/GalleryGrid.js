b3da66203d02b727d2a1c4f82e52560a310a9975
Adrian <adrian>
2025-02-23 15:34:52 +0100

    ficed deploy gallery

--- src/app/galeria/components/GalleryGrid.js
+++ src/app/galeria/components/GalleryGrid.js
@@ -3,7 +3,90 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import FilterSelect from "./FilterSelect";
 import ImageModal from "./ImageModal";
-import Gallery from "@/app/data/gallery";
+
+const Gallery = [
+  {
+    src: '/gallery/baño-centro.jpeg',
+    alt: 'Foto del baño desde el centro en apartamento de Pili',
+    category: 'baño'
+  },
+  {
+    src: '/gallery/baño-puerta.jpeg',
+    alt: 'Foto del baño de la puerta en apartamento de Pili',
+    category: 'baño'
+  },
+  {
+    src: '/gallery/baño-ventana.jpeg',
+    alt: 'Foto del baño desde la ventana en apartamento de Pili',
+    category: 'baño'
+  },
+  {
+    src: '/gallery/cocina-comedor-salon.jpeg',
+    alt: 'Foto de la cocina americana en apartamento de Pili',
+    category: ['cocina', 'comedor', 'salon']
+  },
+  {
+    src: '/gallery/cocina-comedor-ventana.jpeg',
+    alt: 'Foto de la cocina americana vista desde la ventana en apartamento de Pili',
+    category: ['cocina', 'comedor']
+  },
+  {
+    src: '/gallery/comedor-salon.jpeg',
+    alt: 'Foto del comedor en apartamento de Pili',
+    category: ['comedor', 'salon']
+  },
+  {
+    src: '/gallery/comedor.jpeg',
+    alt: 'Foto del comedor en apartamento de Pili',
+    category: 'comedor'
+  },
+  {
+    src: '/gallery/habitacion1-vista-puerta.jpeg',
+    alt: 'Foto de la habitación doble vista desde la puerta en apartamento de Pili',
+    category: 'habitacion 1'
+  },
+  {
+    src: '/gallery/habitacion1-vista-ventana.jpeg',
+    alt: 'Foto de la habitación doble vista desde la ventana en apartamento de Pili',
+    category: 'habitacion 1'
+  },
+  {
+    src: '/gallery/habitacion2-frente.jpeg',
+    alt: 'Foto de la habitación vista desde enfrente en apartamento de Pili',
+    category: 'habitacion 2'
+  },
+  {
+    src: '/gallery/habitacion2-ventana.jpeg',
+    alt: 'Foto de la habitación vista desde la ventana en apartamento de Pili',
+    category: 'habitacion 2'
+  },
+  {
+    src: '/gallery/habitacion2-puerta.jpeg',
+    alt: 'Foto de la habitación vista desde la puerta en apartamento de Pili',
+    category: 'habitacion 2'
+  },
+  {
+    src: '/gallery/habitacion3-centro.jpeg',
+    alt: 'Foto de la habitación vista desde enfrente en apartamento de Pili',
+    category: 'habitacion 3'
+  },
+  {
+    src: '/gallery/habitacion3-cerca.jpeg',
+    alt: 'Foto de la habitación vista desde la ventana en apartamento de Pili',
+    category: 'habitacion 3'
+  },
+  {
+    src: '/gallery/habitacion3-puerta.jpeg', 
+    alt: 'Foto de la habitación vista desde la puerta en apartamento de Pili',
+    category: 'habitacion 3'
+  },
+  {
+    src: '/gallery/salon.jpeg',
+    alt: 'Foto del salón en apartamento de Pili', 
+    category: 'salon'
+  },
+]
+
 
 const GalleryGrid = () => {
   const [selectedCategory, setSelectedCategory] = useState("all");
